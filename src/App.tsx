c028f754efd40629079a6c315116dff961505e1a
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-06-01 16:16:49 +0100

    feat:mui base color

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import themes from './themes'
+import { Typography } from '@mui/material'
+import palette from './themes/palette'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={themes}>
+      <CssBaseline>
+        <header>
+          <Typography component='h2'>React Enterprise</Typography>
+        </header>
+      </CssBaseline>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
